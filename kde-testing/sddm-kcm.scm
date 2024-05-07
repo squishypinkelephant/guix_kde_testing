e37a58a38adb57ea663d3e3c40de83900a395edd
squishypinkelephant <163612839+squishypinkelephant@users.noreply.github.com>
2024-05-06 20:26:54 -0500

    Update sddm-kcm.scm

--- kde-testing/sddm-kcm.scm
+++ kde-testing/sddm-kcm.scm
@@ -23,7 +23,7 @@
 ;;; You should have received a copy of the GNU General Public License
 ;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
 
-(define-module (gnu packages sddm-kcm)
+(define-module (kde-testing packages sddm-kcm)
   #:use-module (guix packages)
   #:use-module (guix download)
   #:use-module (guix git-download)
